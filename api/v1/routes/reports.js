38e8153b55e886cbce610f5df2cd31986a45a4ce
IHIutch <jbhutch01@gmail.com>
2020-10-26 23:24:22 -0400

    add reports path

---
+++ api/v1/routes/reports.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { Report } from "../models/index";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Report.findAll()
+    .then((reports) => {
+      res.json(reports);
+    })
+    .catch((err) => console.log(err));
+});
+
+router.post("/", (req, res) => {
+  const { lat, lng, accelerometer, gyroscope } = req.body;
+
+  Report.create({
+    lat,
+    lng,
+    accelerometer,
+    gyroscope,
+  })
+    .then((data) => {
+      res.status(201).json(data.id);
+    })
+    .catch((err) => console.log(err));
+});
+
+export default router;
